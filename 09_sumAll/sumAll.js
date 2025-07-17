3e37eaca740c76274c581ca2c2599618e277aabd
meynard <sombritomeynard@gmail.com>
2025-07-17 17:26:28 +0800

    test: Pass the first test of sumAll
    This change passes the first test of the sumAll
    spec file.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+/* My algorithm:
+1. Create a function expression that accepts parameters `start` and `end`
+2. Create a for loop where the iterator `i` is equal to `start` and the 
+    condition checks if `i` is less than or equal to `end`, increment step value.
+3. Create a variable sum, initialize to 0.
+4. For every loop, an addition assignment to `sum` is performed where `sum` is 
+    equal to `sum` plus `i`
+5. Return the total `sum`
+*/
 
+const sumAll = function(start, end) {
+  let sum = 0;
+  
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
