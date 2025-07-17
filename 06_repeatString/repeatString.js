fd8ed6eee12fb98883d8476bfd917ab1e0f54c13
meynard <sombritomeynard@gmail.com>
2025-07-17 08:50:19 +0800

    test: Pass the first test of spec file
    This change complies with the first test of the
    spec file where the function 'repeats the string'

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  let repeatedString = "";
+  
+  for (let i = 1; i <= num; i++) {
+    repeatedString += string;
+    // OR
+    // repeatedString = repeatedString.concat(string);
+  }
 
+  return repeatedString;
 };
 
 // Do not edit below this line
