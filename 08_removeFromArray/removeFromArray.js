a599757af679706e2397e27161568382cdb78511
meynard <sombritomeynard@gmail.com>
2025-07-17 11:29:03 +0800

    test: Pass the first test of removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+/*
+1. Create a function expression that accepts parameter `array` and `exclude`
+2. Use .filter() method to filter out the array items that are not equal to the
+    `exclude` parameter
+
+*/
+
+const removeFromArray = function(array, exclude) {
+  return array.filter((item) => item !== exclude);
 };
 
 // Do not edit below this line
