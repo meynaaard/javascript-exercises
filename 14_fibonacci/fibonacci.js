fd0bbd03d05176f5b99ff7bce508f6bf9e741302
meynard <sombritomeynard@gmail.com>
2025-07-26 10:22:16 +0800

    test: Pass 11/11 of fibonacci() function test

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+  // I have not managed to solve this exercise myself :(
+  // Special thanks to Dave Gray for providing an example of both
+  // iterative and recursive fibonacci sequence
 
+  position = parseInt(position);
+
+  if (position < 0) return "OOPS";
+
+  // Iterative approach:
+  /* if (position <= 1) return position;
+  const sequence = [0, 1];
+  for (let i = 2; i <= position; i++) {
+    const [nextToLast, last] = sequence.slice(-2);
+    sequence.push(nextToLast + last);
+  }
+  return sequence[position]; */
+
+  // Recursive approach:
+  if (position < 2) return position;
+  return fibonacci(position - 1) + fibonacci(position - 2);
 };
 
 // Do not edit below this line
