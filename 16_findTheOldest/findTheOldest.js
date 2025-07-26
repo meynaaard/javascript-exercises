74c3914b82416d1f78e12f14be72e63f2fabff16
meynard <sombritomeynard@gmail.com>
2025-07-26 12:53:05 +0800

    test: Pass 3/3 of findTheOldest() function test

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  people.forEach(person => {
+    if ('yearOfDeath' in person) {
+      person.age = person.yearOfDeath - person.yearOfBirth;
+    } else {
+      person.age = (new Date().getFullYear()) - person.yearOfBirth;
+    }
+  });
 
+  const oldestPerson = people.reduce((oldest, person) => {
+    return person.age > oldest.age ? person : oldest
+  });
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
