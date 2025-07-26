7333a024598da2eff2932abb179720dd39b36b83
meynard <sombritomeynard@gmail.com>
2025-07-26 08:32:59 +0800

    test: Pass 8/8 of palindrome function test

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  string = [...string].filter(char => /[^\W_]/ // pattern for non-letter char
+                      .test(char))             // filtered out if true/match
+                      .join("")                // join back as string
+                      .toLowerCase();          // convert to lowercase
 
+  const reversedString = string.split("") // split as an array of char
+                               .reverse() // reverse array
+                               .join(""); // join back as string
+
+  return string === reversedString;
 };
 
 // Do not edit below this line
